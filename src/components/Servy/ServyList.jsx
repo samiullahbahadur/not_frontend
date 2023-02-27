afd4f7d610e3349919ff2fc8754815c3b41bdbc6
Samiullah Bhadur <bahadursamiullah@gmail.com>
2023-02-28 00:39:51 +0430

    Update ServyList.jsx

--- src/components/Servy/ServyList.jsx
+++ src/components/Servy/ServyList.jsx
@@ -1,33 +1,44 @@
-import React from "react";
+
 import classes from "./ServyList.module.css";
-import { useGetUserQuery } from "../../store/userSlice";
+import { Link } from "react-router-dom";
+import { useGetUserQuery, useDeleteUserMutation } from "../../store/userSlice";
 const Card = () => {
-  const { data } = useGetUserQuery();
+  const { data, isLoading, isFetching } = useGetUserQuery();
+  
   return (
     <div className={classes.card}>
       <div className={classes.table_div}>
-        <h4 >list of all Servy</h4>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Sector</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data &&
-              data.map((user) => {
+        <div className={classes.heading}>
+        <h4>list of all Servy</h4>
+        <Link to="form" className={classes.add}>Add New</Link>
+        </div>
+        
+        {isLoading || isFetching ? (
+          <div>Loading</div>
+        ) : (
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Sector</th>
+                <th>Status</th>
+               
+              </tr>
+            </thead>
+            <tbody>
+              {data?.map((user) => {
                 return (
                   <tr key={user.id}>
                     <td>{user.name}</td>
                     <td>{user.sector}</td>
                     <td>{user.status == true ? "Agreed" : ""}</td>
+                    
                   </tr>
                 );
               })}
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
